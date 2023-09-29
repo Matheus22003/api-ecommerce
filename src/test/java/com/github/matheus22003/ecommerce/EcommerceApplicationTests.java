5f52e8f21b82ac7a6bc3684cf12a155601515337
Matheus de Oliveira Simões <45323213+Matheus22003@users.noreply.github.com>
2023-09-29 02:37:13 -0300

    Initial commit

---
+++ src/test/java/com/github/matheus22003/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.matheus22003.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
